9316c7476d1135e842796c9336ce0549e66bd749
anilk52 <yetkinakabal@gmail.com>
2025-10-20 07:46:56 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/coffee/MainActivity.java
+++ app/src/main/java/com/example/coffee/MainActivity.java
@@ -2,13 +2,13 @@ package com.example.coffee;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.coffee.R; // 🔥 Bunu ekle
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); // 📱 XML layout'u bağladık
+        setContentView(R.layout.activity_main); // 📄 XML bağlandı
+        // ☕ XML layout'u bağladık
     }
 }
-
-    
