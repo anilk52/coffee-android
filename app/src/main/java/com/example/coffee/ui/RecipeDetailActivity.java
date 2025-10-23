090ac6cf2a34fef6ecff89a3f506c40987c4a717
anilk52 <yetkinakabal@gmail.com>
2025-10-23 04:46:45 +0100

    Update RecipeDetailActivity.java

--- app/src/main/java/com/example/coffee/ui/RecipeDetailActivity.java
+++ app/src/main/java/com/example/coffee/ui/RecipeDetailActivity.java
@@ -1,16 +1,17 @@
 // app/src/main/java/com/example/coffee/ui/RecipeDetailActivity.java
 package com.example.coffee.ui;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.coffee.R;
 import com.example.coffee.data.RecipesData;
 import com.example.coffee.model.Recipe;
 
-public class RecipeDetailActivity extends Activity {
+public class RecipeDetailActivity extends AppCompatActivity {
 
     private View ingredientsGroup, stepsGroup, tipsGroup;
     private TextView ingredientsView, stepsView, tipsView, titleView;
@@ -21,11 +22,9 @@ public class RecipeDetailActivity extends Activity {
         setContentView(R.layout.activity_recipe_detail);
 
         titleView = findViewById(R.id.titleView);
-
         ingredientsGroup = findViewById(R.id.ingredientsGroup);
         stepsGroup = findViewById(R.id.stepsGroup);
         tipsGroup = findViewById(R.id.tipsGroup);
-
         ingredientsView = findViewById(R.id.ingredientsView);
         stepsView = findViewById(R.id.stepsView);
         tipsView = findViewById(R.id.tipsView);
@@ -46,22 +45,16 @@ public class RecipeDetailActivity extends Activity {
         if (recipe.hasIngredients()) {
             ingredientsView.setText(recipe.getIngredients());
             ingredientsGroup.setVisibility(View.VISIBLE);
-        } else {
-            ingredientsGroup.setVisibility(View.GONE);
-        }
+        } else ingredientsGroup.setVisibility(View.GONE);
 
         if (recipe.hasSteps()) {
             stepsView.setText(recipe.getSteps());
             stepsGroup.setVisibility(View.VISIBLE);
-        } else {
-            stepsGroup.setVisibility(View.GONE);
-        }
+        } else stepsGroup.setVisibility(View.GONE);
 
         if (recipe.hasTips()) {
             tipsView.setText(recipe.getTips());
             tipsGroup.setVisibility(View.VISIBLE);
-        } else {
-            tipsGroup.setVisibility(View.GONE);
-        }
+        } else tipsGroup.setVisibility(View.GONE);
     }
 }
