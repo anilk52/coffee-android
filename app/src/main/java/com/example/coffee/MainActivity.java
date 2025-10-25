01a2e0b84fe4052868463502a342369b6e893a96
anilk52 <yetkinakabal@gmail.com>
2025-10-25 02:34:24 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/coffee/MainActivity.java
+++ app/src/main/java/com/example/coffee/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
         setContentView(R.layout.activity_main);
 
         TextView hello = findViewById(R.id.helloText);
