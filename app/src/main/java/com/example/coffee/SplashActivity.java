d8e2db0405491d50fd183d70afa422aae23b99db
anilk52 <yetkinakabal@gmail.com>
2025-10-25 23:54:56 +0200

    Update SplashActivity.java

--- app/src/main/java/com/example/coffee/SplashActivity.java
+++ app/src/main/java/com/example/coffee/SplashActivity.java
@@ -9,19 +9,17 @@ import android.os.Looper;
 import androidx.core.splashscreen.SplashScreen;
 
 public class SplashActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // Android 12+ sistem splash (ikon/arka plan)
         SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
-
-        // "☕ Selam Barista!" metnini gösteren layout
         setContentView(R.layout.activity_splash);
 
-        // Kısa bekleme sonrası ana ekrana geç
+        // 2 saniye sonra MainActivity’ye geç
         new Handler(Looper.getMainLooper()).postDelayed(() -> {
             startActivity(new Intent(SplashActivity.this, MainActivity.class));
             finish();
-        }, 1500);
+        }, 2000);
     }
 }
