b71af61ffb6350053f1a985546a2dced036af70a
anilk52 <yetkinakabal@gmail.com>
2025-10-27 12:53:17 +0100

    Update Recipe.java

--- app/src/main/java/com/example/coffee/model/Recipe.java
+++ app/src/main/java/com/example/coffee/model/Recipe.java
@@ -1,19 +1,26 @@
-// model/Recipe.java
+package com.example.coffee.model;
+
 public class Recipe {
     private final String name;
-    private final String description;
+    private final String desc;
     private final String category;
     private final int imageResId;
+    private final String size; // “S — 30 ml” gibi
+    private final String tips;
 
-    public Recipe(String name, String description, String category, int imageResId) {
+    public Recipe(String name, String desc, String category, int imageResId, String size, String tips) {
         this.name = name;
-        this.description = description;
+        this.desc = desc;
         this.category = category;
         this.imageResId = imageResId;
+        this.size = size;
+        this.tips = tips;
     }
 
     public String getName() { return name; }
-    public String getDescription() { return description; }
+    public String getDesc() { return desc; }
     public String getCategory() { return category; }
     public int getImageResId() { return imageResId; }
+    public String getSize() { return size; }
+    public String getTips() { return tips; }
 }
