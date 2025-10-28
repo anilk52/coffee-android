22cfd957baa908b7bfd62a0ec88d1523a13e625b
anilk52 <yetkinakabal@gmail.com>
2025-10-28 03:42:42 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/coffee/MainActivity.java
+++ app/src/main/java/com/example/coffee/MainActivity.java
@@ -2,40 +2,86 @@ package com.example.coffee;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import androidx.appcompat.app.AppCompatActivity;
+import android.view.MenuItem;
 
-import com.example.coffee.data.RecipesData;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.coffee.ui.RecipeActivity;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.Arrays;
+import java.util.List;
 
-    @Override protected void onCreate(Bundle savedInstanceState) {
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.btnEspresso).setOnClickListener(this);
-        findViewById(R.id.btnFiltre).setOnClickListener(this);
-        findViewById(R.id.btnSpecial).setOnClickListener(this);
-        findViewById(R.id.btnAlkollu).setOnClickListener(this);
-        findViewById(R.id.btnIce).setOnClickListener(this);
-        findViewById(R.id.btnTurk).setOnClickListener(this);
+        setSupportActionBar(findViewById(R.id.topAppBar));
+
+        rv = findViewById(R.id.rvCategories);
+        rv.setLayoutManager(new GridLayoutManager(this, 2));
+
+        // 6 kategori
+        List<String> cats = Arrays.asList(
+                "Espresso", "Filtre", "Special", "Alkollü", "Ice", "Türk"
+        );
+        rv.setAdapter(new SimpleCategoryAdapter(cats, name -> {
+            Intent i = new Intent(this, RecipeActivity.class);
+            i.putExtra("category", name);
+            startActivity(i);
+        }));
     }
 
-    @Override public void onClick(View v) {
-        int id = v.getId();
-        String cat = null;
-        if (id == R.id.btnEspresso)      cat = RecipesData.CAT_ESPRESSO;
-        else if (id == R.id.btnFiltre)   cat = RecipesData.CAT_FILTRE;
-        else if (id == R.id.btnSpecial)  cat = RecipesData.CAT_SPECIAL;
-        else if (id == R.id.btnAlkollu)  cat = RecipesData.CAT_ALKOLLU;
-        else if (id == R.id.btnIce)      cat = RecipesData.CAT_ICE;
-        else if (id == R.id.btnTurk)     cat = RecipesData.CAT_TURK;
-
-        if (cat != null) {
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_search) {
+            // Basit: RecipeActivity'e kategori olmadan git -> tüm liste
             Intent i = new Intent(this, RecipeActivity.class);
-            i.putExtra("category", cat);
             startActivity(i);
+            return true;
+        } else if (id == R.id.action_favorites) {
+            // Şimdilik placeholder
+            return true;
+        } else if (id == R.id.action_settings) {
+            // Şimdilik placeholder
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    // Basit adapter (ihtiyaç kadar inline)
+    static class SimpleCategoryAdapter extends androidx.recyclerview.widget.RecyclerView.Adapter<VH> {
+        interface OnClick { void onCat(String name); }
+        private final List<String> data;
+        private final OnClick onClick;
+        SimpleCategoryAdapter(List<String> data, OnClick onClick) { this.data = data; this.onClick = onClick; }
+        @NonNull @Override public VH onCreateViewHolder(@NonNull android.view.ViewGroup p, int v) {
+            android.view.View vw = android.view.LayoutInflater.from(p.getContext()).inflate(R.layout.item_category, p, false);
+            return new VH(vw);
+        }
+        @Override public void onBindViewHolder(@NonNull VH h, int pos) {
+            String name = data.get(pos);
+            h.txt.setText(name);
+            h.itemView.setOnClickListener(v -> onClick.onCat(name));
+        }
+        @Override public int getItemCount() { return data.size(); }
+    }
+
+    static class VH extends androidx.recyclerview.widget.RecyclerView.ViewHolder {
+        android.widget.ImageView img;
+        android.widget.TextView txt;
+        VH(@NonNull android.view.View itemView) {
+            super(itemView);
+            img = itemView.findViewById(R.id.img);
+            txt = itemView.findViewById(R.id.txt);
         }
     }
 }
