797a87434f431e71035fc70771085731d1c42720
anilk52 <yetkinakabal@gmail.com>
2025-11-04 23:50:35 +0300

    Create AiBaristaActivity.java

---
+++ app/src/main/java/com/example/coffee/ui/AiBaristaActivity.java
@@ -0,0 +1,16 @@
+package com.example.coffee.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.coffee.R;
+
+public class AiBaristaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ai_barista);
+    }
+}
